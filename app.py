b8cfdc3d87ea9208db2c9490b4166f029cdd4629
Seth J. Connell <sethjordanconnell@gmail.com>
2018-09-22 02:21:09 -0500

    Fixed Buggy Stock Symbol Search
    Will return error if stock symbol is not listed as legit.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #setting up Flask.
 from flask import Flask, render_template, url_for, session, redirect, request
+from yahoo_finance import Share
+import requests
 app = Flask(__name__)
 
 # Defining Route
@@ -19,7 +21,15 @@ def stockinfo():
 
 @app.route("/ticker/<tickervar>")
 def lookupStock(tickervar):
-    return render_template("stockchartexample.html", stocktickername=tickervar)
+    tickervar = tickervar.upper()
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(tickervar)
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == str(tickervar):
+            return render_template("stockchartexample.html", stocktickername=tickervar)
+        else:
+            return "Sorry, no such stock exists."
 # Added Run Option.
 if __name__ == '__main__':
     app.run()
